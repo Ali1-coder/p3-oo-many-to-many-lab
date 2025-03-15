3c98a3dc10e50ac38083fd8fe4f82d996fb3c89b
Ali1-coder <ablhur3@gmail.com>
2025-03-15 22:40:32 +0300

    Class Book

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -3,7 +3,17 @@ class Author:
 
 
 class Book:
-    pass
+    all = []
+    
+    def __init__(self, title):
+        if not isinstance(title, str) or not title.strip():
+            raise Exception("Title must be a non-empty string")
+        self.title = title
+        Book.all.append(self)
+
+    def __repr__(self):
+        return f"Book('{self.title}')"
+
 
 
 class Contract:
