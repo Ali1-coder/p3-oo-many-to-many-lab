e2c904c8533cdcec6d12850e1b61e4670d4bd44b
Ali1-coder <ablhur3@gmail.com>
2025-03-15 22:41:21 +0300

    Class Author and its methods

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,5 +1,27 @@
 class Author:
-    pass
+    all = []
+    
+    def __init__(self, name):
+        if not isinstance(name, str) or not name.strip():
+            raise Exception("Name must be a non-empty string")
+        self.name = name
+        Author.all.append(self)
+
+    def __repr__(self):
+        return f"Author('{self.name}')"
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return list(set(contract.book for contract in self.contracts()))
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
+
 
 
 class Book:
